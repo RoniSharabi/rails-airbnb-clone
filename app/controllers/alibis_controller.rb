3cead9533d936cadb60a684655e7585e082649a4
Roni Sharbi <roni.sherb@gmail.com>
2017-11-27 18:52:33 +0100

    controllers

---
+++ app/controllers/alibis_controller.rb
@@ -0,0 +1,45 @@
+class AlibisController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  before_action :set_alibi, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @alibis = Alibi.all
+  end
+
+  def show
+  end
+
+  def new
+    @alibi = Alibi.new
+  end
+
+  def create
+    @alibi = Alibi.new(alibi_params)
+    if @alibi.save
+      redirect_to alibi_path(@alibi)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @alibi.update(alibi_params)
+    redirect_to alibi_path(@alibi)
+  end
+
+  def destroy
+    @alibi.destroy
+    redirect_to alibis_path
+  end
+
+ private
+
+  def set_alibi
+    @alibi = Alibi.find(params[:id])
+  end
+
+  def alibi_params
+    params.require(:alibi).permit(:title, :description, :price, :location, :category, :user_id)
+  end
+end
